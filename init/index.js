1634fe20d53f6c47434f44931d8918cbfaa36436
abhi-sharma9 <abhisheks.19a@gmail.com>
2026-09-17 04:01:59 +0530

    Initial upload of Wanderlust project

---
+++ init/index.js
@@ -0,0 +1,30 @@
+const mongoose=require("mongoose");
+const initData=require("./data.js");
+const Listing=require("../models/listing.js");
+const MONGO_URL=process.env.MONGO_URL;
+
+main()
+.then(()=>{
+    console.log("connected to DB");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initData.data=initData.data.map((obj)=>({
+        ...obj,
+
+        owner:"6732f015920c0631907077c9",
+    }));
+        await Listing.insertMany(initData.data);
+        console.log("data was initialized");
+};
+
+ initDB();
